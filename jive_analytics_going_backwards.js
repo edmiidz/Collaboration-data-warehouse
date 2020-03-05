a72e9ee9cfc42c7d21241ef68be7b7ad3046721d
Nik Edmiidz <edmiidz@gmail.com>
2020-03-05 13:16:57 -0800

    Removed dateFormat function
    Removed dateFormat function

--- jive_analytics_going_backwards.js
+++ jive_analytics_going_backwards.js
@@ -342,23 +342,6 @@ function insert_redshift(table, data, callback)
     return deferred.promise;
 }
 
-function dateFormat (date, fstr, utc)
-{
-  utc = utc ? 'getUTC' : 'get';
-  return fstr.replace (/%[YmdHMS]/g, function (m) {
-    switch (m) {
-    case '%Y': return date[utc + 'FullYear'] (); // no leading zeros required
-    case '%m': m = 1 + date[utc + 'Month'] (); break;
-    case '%d': m = date[utc + 'Date'] (); break;
-    case '%H': m = date[utc + 'Hours'] (); break;
-    case '%M': m = date[utc + 'Minutes'] (); break;
-    case '%S': m = date[utc + 'Seconds'] (); break;
-    default: return m.slice (1); // unknown code, remove %
-    }
-    // add leading zero if required
-    return ('0' + m).slice (-2);
-  });
-}
 
 function auth(callback)
 {
